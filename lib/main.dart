af83cfd74015e1141029d9a730f7bd2c0aecda9e
RatnagiriRG <ratnagiri557@gmail.com>
2024-12-20 19:01:38 +0530

    phase1 - bug fix

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,21 @@ import 'package:translator/configs/multiprovider.dart';
 import 'package:translator/feature/translator/view/screen/translator_screen.dart';
 
 void main() {
-  runApp(MyAppMultiProvider(
+  runApp(const MyAppMultiProvider(
     myApp: MyApp(),
   ));
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Google Translator',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: TranslatorScreen(),
+      home: const TranslatorScreen(),
     );
   }
 }
